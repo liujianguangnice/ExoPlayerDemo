3d0f28e9334a127dcea725b601cf4cf4a6091f7d
liujianguangnice <liujianguangnice@gmail.com>
2020-01-10 19:49:27 +0800

    添加剪辑视屏，记录播放器状态的日志，以及播放混合文件功能

--- app/src/main/java/com/ed/exoplayerdemo1/MainActivity.java
+++ app/src/main/java/com/ed/exoplayerdemo1/MainActivity.java
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ProgressBar;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlaybackException;
@@ -16,27 +14,38 @@ import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.Player.RepeatMode;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ClippingMediaSource;
 import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DataSpec;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.util.EventLogger;
 import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoListener;
 
+import java.io.IOException;
 import java.util.Formatter;
 import java.util.Locale;
 
@@ -57,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
         initPlayer();
         sourceInjectPlayer();
-        setPlayPause(true);
+        setPlayOrPause(true);
     }
 
     /**
@@ -69,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
             TrackSelection.Factory videoTackSelectionFactory =
                     new AdaptiveTrackSelection.Factory(bandwidthMeter);
-            TrackSelector trackSelector =
+            MappingTrackSelector trackSelector =
                     new DefaultTrackSelector(videoTackSelectionFactory);
             //用于控制MediaSource何时缓冲更多的媒体资源以及缓冲多少媒体资源,在创建播放器的时候被注入
             LoadControl loadControl = new DefaultLoadControl();
@@ -82,7 +91,27 @@ public class MainActivity extends AppCompatActivity {
 
             //添加播放器监听的listener
             mExoPlayer.addListener(eventListener);
-            mExoPlayer.setPlayWhenReady(false);
+            mExoPlayer.addVideoListener(videoListener);
+            //控制媒体是否以及如何循环,也可以通过媒体资源控制循环次数
+            mExoPlayer.setRepeatMode(Player.REPEAT_MODE_ONE);
+            //控制播放列表改组
+            mExoPlayer.setShuffleModeEnabled(false);
+            //调整播放速度和音高setPlaybackParameters
+
+            /**将播放器事件记录到控制台EventLogger，EventLogger|ExoPlayerImpl
+             *EventLogger: state [131.89, 128.27, window=0, period=0, true, ENDED]
+             *[float]：自 player 创建以来的挂钟时间。
+             *[float]：当前播放位置。
+             *[window=int]：当前窗口索引。
+             *[period=int]：该窗口中的当前时段。
+             *[boolean]： playWhenReady标志。
+             *[string]：当前播放状态。
+             *
+             * 当可用或选定的曲目发生变化时，将记录曲目信息。这在回放开始时至少发生一次,轨道tracks和渲染器renderer都会变化，日志会有体现
+             * 播放自适应流时，播放期间会记录正在播放的格式的更改以及所选曲目的属性
+             * */
+            mExoPlayer.addAnalyticsListener(new EventLogger(trackSelector));
+
         }
     }
 
@@ -90,7 +119,8 @@ public class MainActivity extends AppCompatActivity {
      * 加载MediaSource数据
      */
     private void sourceInjectPlayer() {
-        ConcatenatingMediaSource videoSource = buildMediaSource();
+        //ConcatenatingMediaSource videoSource = buildMediaSource();
+        MediaSource videoSource = clippingMediaSource();
         if (mExoPlayer != null) {
             //MediaSource在播放开始的时候，通过ExoPlayer.prepare方法注入
             mExoPlayer.prepare(videoSource);
@@ -98,9 +128,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * 播放或停止播放器
+     * 开始和暂停播放
      */
-    private void setPlayPause(boolean play) {
+    private void setPlayOrPause(boolean play) {
         if (mExoPlayer != null) {
             mExoPlayer.setPlayWhenReady(play);
         }
@@ -133,9 +163,41 @@ public class MainActivity extends AppCompatActivity {
 
         MediaSource videoSource1 = new ExtractorMediaSource(uri1, dataSourceFactory, extractorsFactory,
                 null, null);
-        return new ConcatenatingMediaSource(videoSource, videoSource1);
+        return new ConcatenatingMediaSource(videoSource);
 
     }
+
+
+    /**
+     * 剪辑某段视频
+     * 示例将视频播放剪辑为以5秒开始并以10秒结束
+     */
+    private MediaSource clippingMediaSource(){
+        Uri uri = Uri.parse(
+                "http://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4");
+
+        //测量播放过程中的带宽。 如果不需要，可以为null。自适应流的核心就是选择最合适当前播放环境的轨道,自适应播放根据测量的下载速度来估计网络带宽
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        // 生成加载媒体数据的DataSource实例。
+        DataSource.Factory dataSourceFactory
+                = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "useExoplayer"), bandwidthMeter);
+        // 生成用于解析媒体数据的Extractor实例。
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        MediaSource videoSource = new ExtractorMediaSource(uri, dataSourceFactory, extractorsFactory,
+                null, null);
+        // Clip to start at 5 seconds and end at 10 seconds.
+        ClippingMediaSource clippingSource =
+                new ClippingMediaSource(
+                        videoSource,
+                        /* startPositionUs= */ 5_000_000,
+                        /* endPositionUs= */ 15_000_000);
+
+        // 控制播放的次数，前提是mExoPlayer.setRepeatMode不能做设置
+        //LoopingMediaSource loopingSource = new LoopingMediaSource(clippingSource, 2);
+        return clippingSource;
+    }
+
 
     /**
      * 播放器监听器
@@ -147,7 +209,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-            Log.i(TAG, "onTracksChanged:播放器轨道选择器切换...");
+            Log.i(TAG, "onTracksChanged:播放器轨道选择器切换中...");
         }
 
         @Override
@@ -157,25 +219,25 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-            Log.i(TAG, "onPlayerStateChanged播放状态变化中: 准备工作完成是否播放 = " + String.valueOf(playWhenReady)
+            Log.i(TAG, "播放器状态变化: 准备工作完成是否播放 = " + String.valueOf(playWhenReady)
                     + " 当前播放器状态 = " + playbackState);
             switch (playbackState) {
                 case ExoPlayer.STATE_ENDED:
-                    Log.i(TAG, "Playback ended!");
+                    Log.i(TAG, "播放器播放完所有媒体");
                     //Stop playback and return to start position
-                    setPlayPause(false);
                     mExoPlayer.seekTo(0);
+                    setPlayOrPause(false);
                     break;
                 case ExoPlayer.STATE_READY:
-                    Log.i(TAG, "播放器准备完毕! pos: " + mExoPlayer.getCurrentPosition()
-                            + " max: " + stringForTime((int) mExoPlayer.getDuration()));
+                    Log.i(TAG, "播放器准备完毕，当前位置: " + mExoPlayer.getCurrentPosition()
+                            + " 最大时长: " + stringForTime((int) mExoPlayer.getDuration()));
                     setProgress(0);
                     break;
                 case ExoPlayer.STATE_BUFFERING:
-                    Log.i(TAG, "Playback buffering!");
+                    Log.i(TAG, "播放器当前位置无法立即进行播放，数据加载中..");
                     break;
                 case ExoPlayer.STATE_IDLE:
-                    Log.i(TAG, "ExoPlayer idle!");
+                    Log.i(TAG, "播放器初始状态，播放器停止或播放失败");
                     break;
             }
         }
@@ -183,6 +245,26 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onPlayerError(ExoPlaybackException error) {
             Log.i(TAG, "onPlaybackError: " + error.getMessage());
+            if (error.type == ExoPlaybackException.TYPE_SOURCE) {
+                IOException cause = error.getSourceException();
+                if (cause instanceof HttpDataSource.HttpDataSourceException) {
+                    // An HTTP error occurred.
+                    HttpDataSource.HttpDataSourceException httpError = (HttpDataSource.HttpDataSourceException) cause;
+                    // This is the request for which the error occurred.
+                    DataSpec requestDataSpec = httpError.dataSpec;
+                    // It's possible to find out more about the error both by casting and by
+                    // querying the cause.
+                    if (httpError instanceof HttpDataSource.InvalidResponseCodeException) {
+                        // Cast to InvalidResponseCodeException and retrieve the response code,
+                        // message and headers.
+                    } else {
+                        // Try calling httpError.getCause() to retrieve the underlying cause,
+                        // although note that it may be null.
+                    }
+
+                    android.util.Log.i(TAG, "onPlayerError:HTTP网络问题导致播放失败 ");
+                }
+            }
         }
 
         public void onPositionDiscontinuity() {
@@ -194,6 +276,25 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, "MainActivity.onPlaybackParametersChanged." + playbackParameters.toString());
         }
     };
+
+
+    private VideoListener videoListener = new VideoListener() {
+        @Override
+        public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+            android.util.Log.i(TAG, "onVideoSizeChanged: ");
+        }
+
+        @Override
+        public void onSurfaceSizeChanged(int width, int height) {
+            android.util.Log.i(TAG, "onSurfaceSizeChanged: ");
+        }
+
+        @Override
+        public void onRenderedFirstFrame() {
+            android.util.Log.i(TAG, "onRenderedFirstFrame: ");
+        }
+    };
+
 
     /**
      * 时间格式化
@@ -224,7 +325,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.i(TAG, "onResume.");
         if (mExoPlayer != null) {
-            setPlayPause(true);
+            setPlayOrPause(true);
         }
     }
 
@@ -233,7 +334,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "onPause.");
         super.onPause();
         if (mExoPlayer != null) {
-            setPlayPause(false);
+            setPlayOrPause(false);
         }
     }
 
@@ -242,7 +343,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "onStop.");
         super.onStop();
         if (mExoPlayer != null) {
-            setPlayPause(false);
+            setPlayOrPause(false);
         }
 
 
